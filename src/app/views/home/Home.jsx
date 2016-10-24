4c106e834591a63a6059f153346e3b47d47d3399
simranraza <s.imran.raza@gmail.com>
2016-10-24 15:20:28 +1100

    test

--- src/app/views/home/Home.jsx
+++ src/app/views/home/Home.jsx
@@ -2,10 +2,13 @@ import React      from 'react';
 import Jumbotron  from '../../components/jumbotron/Jumbotron.jsx';
 import classNames from 'classnames';
 import { Link }   from 'react-router';
+import Request    from 'superagent';
+import _          from 'lodash';
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {movies:[],searchText:''}
     this.init();
   }
 
@@ -17,9 +20,11 @@ class Home extends React.Component {
   }
 
   componentWillMount() {
-    this.state = {
+        this.search();
+      this.state = {
       viewEnters  : true
     };
+    
   }
 
   processViewAnimationClasses() {
@@ -30,37 +35,47 @@ class Home extends React.Component {
     return homeViewClasses;
   }
 
+    updateSearch = function(st){
+        console.log(this.state.searchText);
+        this.search(this.state.searchText);
+    }
+    
+    searchTextChange= function(e) {
+        console.log(e.target.value);
+        this.setState({searchText: e.target.value});
+    }
   render() {
+     
+      var movies = _.map(this.state.movies, (movie)=>{
+          return <li>{movie.Title}</li>
+      })
+      
     return(
       <div
         key="homeView"
         className={this.processViewAnimationClasses()}>
-        <Jumbotron>
-          <h1>
-            Full ES2015 ReactJS + Bootstrap
-          </h1>
-          <h2>
-            with Hot Reload!!!
-          </h2>
-          <h2>
-            with React Router (SPA)
-          </h2>
-          <h1>
-            Starter
-          </h1>
-          <h1></h1>
-          <p>
-            <Link
-              className="btn btn-success btn-lg"
-              to={'/about'}>
-              <i className="fa fa-info"></i>
-              &nbsp;
-              go to about
-            </Link>
-          </p>
-        </Jumbotron>
+        <div>
+            <form>
+                <input type="text" name="searchText" onChange={this.searchTextChange.bind(this)}></input>
+                <input type="button" name="searchButton" value="Search" onClick={this.updateSearch.bind(this)}></input>
+            </form>
+        </div>
+        <div>
+             <ul>{movies}</ul> 
+        </div>    
       </div>
     );
+  }
+    
+    search(query = "star"){
+    var url = `http://www.omdbapi.com?s=${query}&y=&r=json&plot=short`;
+    Request.get(url).then((response) => {
+        console.log(response);
+      this.setState({
+        movies: response.body.Search,
+        total: response.body.totalResults
+      });
+    });
   }
 }
 
